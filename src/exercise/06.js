420d785407d7129c5466691ae243c70f5ed8c031
vadimqq <vadimsilan199@gmail.com>
2021-04-23 16:41:47 +0400

    add hudleSubmit function

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -8,7 +8,10 @@ function UsernameForm({onSubmitUsername}) {
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
-  //
+  const handleSubmit = (event) => {
+		event.preventDefault()
+		onSubmitUsername(event.target.elements[0].value)
+	}
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
@@ -19,12 +22,12 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={onSubmitUsername}>
       <div>
         <label>Username:</label>
         <input type="text" />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
     </form>
   )
 }
